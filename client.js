d73cae90fe8d198e5313e01c039e4e2fcbab1532
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-23 20:56:06 +0000

    Added two new events in the connect function to print a message when client is successfully connected and a name message

--- client.js
+++ client.js
@@ -16,7 +16,51 @@ const net = require('net');
      console.log('Incoming:', data);
    })
 
+   conn.on('connect', () => {  
+    // when a connection is established  
+      console.log("Successfully connected to server!");
+      //conn.write("Name: AVK")
+    });
+
+    // 
+    conn.on('connect', () => {
+      conn.write("Name: AVK");
+    })
+
    return conn;
  }
 
  module.exports = connect
+
+
+
+//  const net = require('net');
+// const stdin = process.stdin;
+
+// stdin.setEncoding('utf8');
+// stdin.resume();
+
+// const client = net.createConnection({  
+//   host: "2.tcp.ngrok.io",  
+//   port: 16543
+// });
+
+// stdin.on('data', (data) => {  
+//   if (data === '\\q\n') {    
+//     process.exit();  
+//   }  
+//     client.write(data);
+//   });
+  
+//   client.setEncoding('utf8');
+  
+//   client.on('connect', () => {  
+//     // when a connection is established  
+//     console.log("successfully connected to server");  
+//     client.write('hello this is elon musk');
+  
+//   });
+  
+//   client.on('data', (data) => {  
+//     console.log('Incomming:', data);
+//   });
