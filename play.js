d00da160a1348a3ff949cac6823d8ca5eeb995eb
Avvai Ketheeswaran <avvai.kethees@gmail.com>
2020-09-23 22:06:22 +0000

    Set up stdin by creating two function. The setupInput function sets up the stdin and the handleUserInput function allows client to exit out of the game with control c

--- play.js
+++ play.js
@@ -1,3 +1,28 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+
+ const setupInput = function () {
+   const stdin = process.stdin;
+   stdin.setRawMode(true);
+   stdin.setEncoding('utf8');
+   stdin.resume();
+
+   const handleUserInput = function () {
+     stdin.on('data', (data) => {
+    if (data == '\u0003') {
+      process.exit();
+    }
+  });
+  }
+  handleUserInput ();
+   return stdin;
+ }
+
+ setupInput();
+
